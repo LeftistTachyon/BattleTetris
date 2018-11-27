aad04a79990b9a8fc6187cf0d06da32d1cfe285a
Gui Guy <jedandeganwang1999@gmail.com>
2018-11-27 13:36:11 -0600

    :Trying to remove lag

--- BattleTetrisClient/src/simpletetris/AudioPlayer.java
+++ BattleTetrisClient/src/simpletetris/AudioPlayer.java
@@ -289,7 +289,6 @@ public class AudioPlayer {
     public static void playMoveSFX(double volume) {
         if(!initialized) initialize();
         
-        System.out.println(MOVE == null);
         sfxPlayer = new MediaPlayer(MOVE);
         sfxPlayer.setVolume(volume);
         sfxPlayer.play();
